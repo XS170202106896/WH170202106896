d5e12e0d6f2baefee39c5cd38ca6ffeb0758af26
Andiy <2797564608@qq.com>
2019-05-16 22:28:59 +0800

    sda

---
+++ src/demoProj/jiaowu.java
@@ -0,0 +1,11 @@
+package demoProj;
+
+public class jiaowu {
+	public static void main(String[] args) {
+
+		System.out.println("good branch!");
+
+		System.out.println("good!");
+
+	}
+}
---
+++ src/demoProj/student.java
@@ -0,0 +1,16 @@
+package demoProj;
+
+public class student {
+
+	public String name;
+	   public String address;
+	   student(){
+		   
+
+	   public String name;
+	   public String address;
+	   
+	   student(){   
+
+	   }
+}
